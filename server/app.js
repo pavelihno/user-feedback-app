9cf0b5bd2d358717292c3765a6b08db12940e9fb
pavelihno <crazzyyellow02@gmail.com>
2023-04-22 23:19:06 +0300

    server side connected to db

--- server/app.js
+++ server/app.js
@@ -1,9 +1,29 @@
-const express = require('express');
+import express from 'express';
+import mongoose from 'mongoose';
+
+
+const connectDB = async () => {
+  const MONGO_URI = `mongodb://${process.env.MONGODB_ROOT_USER}:${process.env.MONGODB_ROOT_USER_PASSWORD}@mongo:${process.env.MONGODB_DOCKER_PORT}/${process.env.MONGODB_DATABASE}`;
+  
+  await mongoose
+    .connect(MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => console.log("Successfully connected to DB"))
+    .catch((e) => {
+      console.log(`Error while connecting to DB: ${e.message}`);
+    });
+};
+
 const app = express();
-const port = process.env.EXPRESS_DOCKER_PORT || 4000;
+
+connectDB();
+
 app.get('/', (req, res) => {
   res.send('Home Route');
 });
-app.listen(port, () =>
-  console.log(`Server running on port ${port}, http://localhost:${port}`)
-);
+
+
+
+export default app;
---
+++ server/server.js
@@ -0,0 +1,9 @@
+import app from "./app.js";
+import http from 'http';
+
+const port = process.env.EXPRESS_DOCKER_PORT || 4000;
+const server = http.createServer(app);
+
+server.listen(port, () =>
+  console.log(`Server running on port ${port}, http://localhost:${port}`)
+);
